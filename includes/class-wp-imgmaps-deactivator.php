1527f1d67eae2d1b4c25799f98daa641a074b19e
iankevinmcdonald <ian@mcdonald.me.uk>
2018-01-18 15:08:59 +0000

    Added Boilerplate
    Added boilerplate from WPBM.me

---
+++ includes/class-wp-imgmaps-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://mcdonald.me.uk/
+ * @since      1.0.0
+ *
+ * @package    Wp_Imgmaps
+ * @subpackage Wp_Imgmaps/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Imgmaps
+ * @subpackage Wp_Imgmaps/includes
+ * @author     Ian McDonald <ian@mcdonald.me.uk>
+ */
+class Wp_Imgmaps_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
