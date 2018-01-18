25e28776c801416c99f11824b2130e22bad0bc33
Ian <ian@orwell.(none)>
2018-01-18 19:18:21 +0000

    Committing the new biolerplate files with the new add-img-maps stub

--- uninstall.php
+++ uninstall.php
@@ -19,10 +19,10 @@
  * For more information, see the following discussion:
  * https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate/pull/123#issuecomment-28541913
  *
- * @link       http://mcdonald.me.uk/
+ * @link       mcdonald.me.uk
  * @since      1.0.0
  *
- * @package    Wp_Imgmaps
+ * @package    Add_Img_Maps
  */
 
 // If uninstall not called from WordPress, then exit.
