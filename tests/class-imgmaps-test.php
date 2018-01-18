f331bd49b19af02a7026687a2cb8fb7776c81d24
iankevinmcdonald <ian@mcdonald.me.uk>
2018-01-18 16:29:39 +0000

    Unit tests

---
+++ tests/class-imgmaps-test.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace wordpress-imagemaps;
+
+use ImgMaps;
+use ImgMaps_Map;
+use PHPUnit_Framework_TestCase;
+
+class ImgMaps_Test extends PHPUnit_Framework_TestCase {
+	// Nothing here yet!
+}
+
+class ImgMaps_Map_Test extends PHPUnit_Framework_TestCase {
+	// Nothing here yet!
+}
